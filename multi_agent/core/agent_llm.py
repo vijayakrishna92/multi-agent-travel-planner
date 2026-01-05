5309edf3e7da108ead818d7a5432b3309f07ee38
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2026-01-05 21:06:28 +0530

    add openai requirement example env

--- multi_agent/core/agent_llm.py
+++ multi_agent/core/agent_llm.py
@@ -4,6 +4,7 @@ import os
 import getpass
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+#from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -21,3 +22,20 @@ def get_llm(
         api_key=os.environ.get("GROQ_API_KEY"),
         temperature=temperature
     )
+
+# openai
+
+# def get_llm(
+#     model: str = "gpt model name goes here",
+#     temperature: float = 0.1 # range from 0.1 to 0.9 higher more creative
+# ):
+#     if "OPENAI_API_KEY" not in os.environ:
+#         os.environ["OPENAI_API_KEY"] = getpass.getpass(
+#             "Enter your openai API key: "
+#         )
+
+#     return ChatOpenAI(
+#         model=model,
+#         api_key=os.environ.get("OPENAI_API_KEY"),
+#         temperature=temperature
+#     )
